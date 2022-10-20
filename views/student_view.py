4d27b18d0cf17775164b04e582b902df99cbb84f
Howard Yin <3309763881@qq.com>
2022-10-20 21:56:16 +0800

    Update student_view.py

--- views/student_view.py
+++ views/student_view.py
@@ -6,12 +6,13 @@ Version:  V 1.0
 File:     student_view
 """
 import json
+import logging
 
 from flask import Blueprint, render_template, request, g, redirect, url_for, flash, session, jsonify
 from werkzeug.security import check_password_hash
 
 from common.ext import db
-from models import Student
+from models import Student, Subject, ReleaseSubject
 import pickle
 
 bp = Blueprint("sbp", __name__, url_prefix='/student')
@@ -28,9 +29,10 @@ def index():
 @bp.route('/login', methods=['POST'])
 def login():
     form = request.json
-    username = form['username']
+    student_id = form['username']
     password = form['password']
-    student = db.session.query(Student).filter_by(student_id=username).first()
+    session['student_id'] = student_id
+    student = db.session.query(Student).filter_by(student_id=student_id).first()
     if student and check_password_hash(student.password, password):
         data = row2dict(student)
         data['code'] = '200'
@@ -49,19 +51,44 @@ def row2dict(row):
     return d
 
 
-# 用户信息界面
-@bp.route('/info')
+# 用户信息界面 获取个人信息
+@bp.route('/info', methods=['GET', 'POST'])
 def info():
     student_id = session['student_id']
     student = db.session.query(Student).filter_by(student_id=student_id).first()
-    context = {'student': student}
-    return render_template('student_info.html', **context)
+    if request.method == 'GET':
+        data = row2dict(student)
+        data['code'] = '200'
+        return jsonify(data)
+    # 更改信息
+    else:
+        # update student info
+        form = request.json
+        # form为提交的表单内容 student_new 为更改后的学生信息
+        for key in form.keys():
+            # 更改学生信息
+            setattr(student, key, form[key])
+            # 打印日志
+            logging.warning(f'student {student.name}\'s {key} changed to {form[key]} ')
+        db.session.commit()
+        data = row2dict(student)
+        data['code'] = '200'
+        return jsonify(data)
 
 
 # 课题市场界面
-@bp.route('/subject_market')
+@bp.route('/subject_market', methods=['GET', 'POST'])
 def subject_market():
-    pass
+    # if get
+    # 查询所有已经发布的课题
+    subjects = db.session.query(Subject, ReleaseSubject).join(ReleaseSubject).filter_by(
+        ReleaseSubject.released).limit(20, 0).all()
+    data = dict()
+    for subject in subjects:
+        subject_data = row2dict(subject)
+        data[subject.subject_id] = subject_data
+    data['code'] = '200'
+    return jsonify(data)
 
 
 # 题目愿望单
