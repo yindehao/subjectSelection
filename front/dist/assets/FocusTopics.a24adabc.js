2778bfb6e5f02fe907dc85ad2d78a172f025bd81
Howard Yin <3309763881@qq.com>
2022-10-21 14:53:39 +0800

    delete front files

--- front/dist/assets/FocusTopics.a24adabc.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o,g as e}from"./index.17f5108c.js";const s={};function n(r,t){return o(),e("div",null," FoucusTopics ")}const a=c(s,[["render",n]]);export{a as default};
--- front/dist/assets/MyGroup.f1837fea.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,g as r}from"./index.17f5108c.js";const c={};function n(t,s){return o(),r("div",null," MyGroup ")}const a=e(c,[["render",n]]);export{a as default};
